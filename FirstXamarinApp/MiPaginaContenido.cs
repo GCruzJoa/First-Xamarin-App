e96becd7ba0d97c972c1e798c2583415e2df6dc9
Gabriel Cruz <gabriel.cruzjoa@gmail.com>
2021-02-12 00:37:46 -0400

    Xamarin App

---
+++ FirstXamarinApp/MiPaginaContenido.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FirstXamarinApp
+{
+    class MiPaginaContenido : ContentPage
+    {
+        public MiPaginaContenido()
+        {
+            var label = new Label
+            {
+                Text = "Escribe tu nombre"
+            };
+            var txtNombre = new Entry
+            {
+                Placeholder = "Escribe tu nombre"
+            };
+            Content = new StackLayout
+            {
+                Padding = 30,
+                Spacing = 10,
+                Children = { label, txtNombre }
+            };
+        }
+        
+    }
+}
